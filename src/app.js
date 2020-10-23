ab9239dd74ee1273b69d613310e1e1b1252aa824
sam p <spoveda545@gmail.com>
2020-10-23 17:57:27 -0400

    finished error handler

--- src/app.js
+++ src/app.js
@@ -8,12 +8,20 @@ const bookmarkRouter = require('./routes/bookmark.route');
 
 const app = express();
 
-const morganOptn = (NODE_ENV === 'production') ? 'tiny' : 'common';
-
-app.use(morgan(morganOptn));
+app.use(morgan((()=>(NODE_ENV === 'production') ? 'tiny' : 'common')()));
 app.use(helmet());
 app.use(cors());
+app.use('/bookmark', bookmarkRouter);
 
-app.use('/bookmark', bookmarkRouter)
+//catch-all error handler
+app.use((error, req, res, next)=>{
+	let resp = null;
+	if (NODE_ENV === 'production') {
+		resp = { error: { message: 'server error' } };
+	} else {
+		resp = { message: error.message, error };
+	}
+	res.status(500).json(resp);
+});
 
 module.exports = app;
